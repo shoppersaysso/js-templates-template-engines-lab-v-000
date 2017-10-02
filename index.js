eacb4a70e87e38df14054d3134717e9455400d43
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 03:10:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+}
